abf174b6666b1f99454292e700491ed3a4997f0c
Andreas Luedecke <andreas.luedecke@comparex.com>
2019-04-16 18:07:33 +0200

    added MailingInterface

---
+++ ALMailing/MailingInterface.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ALMailing
+{
+    public interface MailingInterface
+    {
+        #region Properties
+        IEnumerable<MailMessage> Mail { get; set; }
+        IEnumerable<NetworkCredential> Credential { get; set; }
+        IEnumerable<SmtpClient> Smptp { get; set; }
+        #endregion
+
+        #region Methods
+        void SendSingleMail(SmtpClient smtp, MailMessage mail);
+        #endregion
+    }
+}
